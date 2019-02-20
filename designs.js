dc55bfc5460df1bf570ead81446b788e5df1435e
Priscilla Kaendler <28449238+priscillaka@users.noreply.github.com>
2019-02-20 14:17:00 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,28 +1,34 @@
-var height = 0;
-var width = 0;
+
 
 $('#sizePicker').submit(function(e) {
+//Prevents page to reload
     e.preventDefault();
-    height = $('#inputHeight').val()
-    width = $('#inputWidth').val()
 
+//Set the grid dimensions
+    var height = $('#inputHeight').val()
+    var width = $('#inputWidth').val()
+
+//Calls the makeGrid() function
     makeGrid(height, width)
 });
 
 
-
+//Builds the grid
 function makeGrid(height, width) {
-    $('#pixelCanvas').empty();
+    $('#pixelCanvas').empty();  //Clears the grid and starts over
 
-    for(var i = 0; i <= height; i++) {
+//Loop to insert the rows
+    for(var i = 0; i < height; i++) {
         $('#pixelCanvas').append('<tr class="row"></tr>');
     }
 
-    for(var i = 0; i <= width; i++) {
+//Loop to insert the columns
+    for(var i = 0; i < width; i++) {
         $('.row').append('<td></td>');
     }
 
 
+// here is where the colorPicker is used to color each cell
     $('td').click(function addColor() {
         if($(this).attr("style")) {
             $(this).removeAttr("style");
